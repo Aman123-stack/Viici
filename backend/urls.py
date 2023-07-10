794ed0e1d4c2887679e4ac789d9daf27daf562ba
Aman Swastik Rath <amanrath567@gmail.com>
2023-07-11 01:12:55 +0530

    post api thing changed

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from backend.views import UserRegistrationView,UserLoginView,UserProfileView
+from backend.views import UserRegistrationView,UserLoginView,UserProfileView,PostCreateView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(),name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('<int:userid>/post',PostCreateView.as_view(),name='Post'),
 ]
